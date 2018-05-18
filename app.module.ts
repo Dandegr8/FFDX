bec61400a46f6dd94906433ca1eb948e3ce4889a
Dandegr8 <Dandegr8@users.noreply.github.com>
2018-05-18 18:16:28 -0400

    work and contacts components

--- app.module.ts
+++ app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { WorkComponent } from './work/work.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule,
