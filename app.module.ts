6ac32b1ec92dfe2ef2a51bf856e04a2aec624aa1
Farid223 <farid_shaikh@hotmail.com>
2018-05-26 20:25:52 -0400

    hello

--- app.module.ts
+++ app.module.ts
@@ -14,20 +14,24 @@ import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 import { HeroesComponent }      from './heroes/heroes.component';
 import { HeroSearchComponent }  from './hero-search/hero-search.component';
 import { MessagesComponent }    from './messages/messages.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material';
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-
+    BrowserAnimationsModule,
+    MatButtonModule
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
     // Remove it when a real server is ready to receive requests.
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    )
+   // HttpClientInMemoryWebApiModule.forRoot(
+     // InMemoryDataService, { dataEncapsulation: false }
+   // ),
+
+   
   ],
   declarations: [
     AppComponent,
